b62384dab137ed0b3f7a969b0844e28c2c990e01
Santiago Enciso <santiago.enciso@extend.com>
2025-01-24 17:06:49 -0600

    [SOL-699] Updates version of cypress

--- cypress.config.js
+++ cypress.config.js
@@ -8,6 +8,13 @@ module.exports = defineConfig({
     screenshotsFolder: 'cypress/screenshots',
     videosFolder: 'cypress/videos',
     supportFile: false,
-    env: require('./cypress.env.json'),
+    env: {
+      SITE_URL: process.env.CYPRESS_SITE_URL,
+      WP_ADMIN_USERNAME: process.env.CYPRESS_WP_ADMIN_USERNAME,
+      WP_ADMIN_PASSWORD: process.env.CYPRESS_WP_ADMIN_PASSWORD,
+      STORE_ID: process.env.CYPRESS_STORE_ID,
+      CLIENT_ID: process.env.CYPRESS_CLIENT_ID,
+      CLIENT_SECRET: process.env.CYPRESS_CLIENT_SECRET
+    }
   }
 });
