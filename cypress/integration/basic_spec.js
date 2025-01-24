ddfe4089133a8d8b22ac8f029dfb6cf4719c066d
Santiago Enciso <santiago.enciso@extend.com>
2025-01-24 14:24:17 -0600

    [SOL-699] Deleted extra spec file. Ready to push to master.

--- cypress/integration/basic_spec.js
+++
@@ -1,6 +0,0 @@
-describe('Basic WooCommerce Test', () => {
-    it('Visits the test site', () => {
-        cy.visit('https://woocommerce.woodys.extend.com/');
-        cy.contains('WooCommerce').should('exist');
-    });
-});
