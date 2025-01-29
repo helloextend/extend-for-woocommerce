9ad5e2853aa466489f6ee93ba3000ee7861a618f
Santiago Enciso <santiago.enciso@extend.com>
2025-01-29 16:36:57 -0600

    [SOL-700] First Cypress test

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,19 @@
-const { defineConfig } = require('cypress');
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://woocommerce.woodys.extend.com/',
+    baseUrl: "https://woocommerce.woodys.extend.com",
     specPattern: 'cypress/integration/**/*.cy.{js,jsx,ts,tsx}',
     fixturesFolder: 'cypress/fixtures',
     screenshotsFolder: 'cypress/screenshots',
     videosFolder: 'cypress/videos',
-    supportFile: false
-  }
+    supportFile: false,
+    env: {
+      WP_ADMIN_USERNAME: process.env.WP_ADMIN_USERNAME,
+      WP_ADMIN_PASSWORD: process.env.WP_ADMIN_PASSWORD,
+      STORE_ID: process.env.STORE_ID,
+      CLIENT_ID: process.env.CLIENT_ID,
+      CLIENT_SECRET: process.env.CLIENT_SECRET,
+    },
+  },
 });
