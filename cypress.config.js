de5d31da7ffdfd274faeddb64711ff271b46ac80
Santiago Enciso <santiago.enciso@extend.com>
2025-01-29 16:14:31 -0600

    [SOL-699] cleaned up env variables from Cypress config file

--- cypress.config.js
+++ cypress.config.js
@@ -7,14 +7,6 @@ module.exports = defineConfig({
     fixturesFolder: 'cypress/fixtures',
     screenshotsFolder: 'cypress/screenshots',
     videosFolder: 'cypress/videos',
-    supportFile: false,
-    env: {
-      CYPRESS_SITE_URL: process.env.SITE_URL,
-      CYPRESS_WP_ADMIN_USERNAME: process.env.WP_ADMIN_USERNAME,
-      CYPRESS_WP_ADMIN_PASSWORD: process.env.WP_ADMIN_PASSWORD,
-      CYPRESS_STORE_ID: process.env.STORE_ID,
-      CYPRESS_CLIENT_ID: process.env.CLIENT_ID,
-      CYPRESS_CLIENT_SECRET: process.env.CLIENT_SECRET
-    }
+    supportFile: false
   }
 });
