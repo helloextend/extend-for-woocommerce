f553420b017d28ceeb54876bfad7e443fde5d57f
Santiago Enciso <santiago.enciso@extend.com>
2025-02-10 14:55:21 -0600

    [SOL-700] Post purchase tests. Goes to wp-admin and sets the last order to 'completed' status to trigger PP contract creation.

--- cypress/e2e/example_spec.cy.js
+++
@@ -1,8 +0,0 @@
-// cypress/e2e/shipping_protection.cy.js
-
-describe('Cypress Test', () => {
-    it('Visits the Extend Warranty site', () => {
-        cy.visit('https://woocommerce.woodys.extend.com');
-        cy.contains('WooCommerce').should('be.visible');
-    });
-});
